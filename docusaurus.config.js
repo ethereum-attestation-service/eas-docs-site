71caeb2a51a325ac3af2eae6bb22edf67f73b091
Steve Dakh <slavik0329@gmail.com>
2023-05-27 10:37:36 -0400

    fix edit config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ethereum-attestation-service/eas-docs-site',
+            'https://github.com/ethereum-attestation-service/eas-docs-site/blob/main/',
         },
         blog: {
           showReadingTime: true,
